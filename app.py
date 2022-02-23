87f4ad2ee832e0f98dc79f444b257a5861eda935
reload1bronze <nature9th@gmail.com>
2022-02-23 10:20:27 +0900

    첫 화면(MBTI 통계) - DB에서 유저데이터 가져온 후 MBTI별 인원 정리

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
 from flask import Flask, render_template, jsonify
-import requests
-from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-url = 'http://www.yes24.com/24/category/bestseller'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get(url, headers=headers)
-soup = BeautifulSoup(data.text, 'html.parser')
+client = MongoClient(
+    'mongodb+srv://test:sparta@cluster0.cisbf.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 
-best_list = soup.select('#bestList > ol > li')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route('/bestseller', methods=["GET"])
-def best_get():
-    sorted_list = []
-    for best in best_list:
-        img_url = best.select_one('p:nth-child(2) > a > img')['src']
-        title = best.select_one('p:nth-child(3) > a').text
-        sorted_list.append({'img_url': img_url, 'title': title})
-    print(sorted_list)
-    return jsonify({'best_list': sorted_list})
+@app.route("/mbti", methods=["GET"])
+def user_get():
+    user_list = list(db.hhnn.find({}, {'_id': False}))
+    print(user_list)
+    return jsonify({'user_list': user_list})
 
 
 if __name__ == '__main__':
